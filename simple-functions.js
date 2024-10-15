258cd5b5de92d850c4bea973a2301faddde2b368
Nicholas Lynch <nicklynch820@gmail.com>
2024-10-15 11:58:00 -0400

    simple functions

---
+++ simple-functions.js
@@ -0,0 +1,18 @@
+"use strict"
+
+function favoriteThings(){
+console.log("Nicholas James Lynch");
+console.log("Goodfellas");
+console.log("Kendrick Lamar");
+}
+favoriteThings()
+
+function whyImHere(){
+    console.log("I joined Year Up United to learn more about coding and have a better opportunity to find a job in the tech field.");
+}
+whyImHere()
+
+function favoritePlaces() {
+    console.log("One of my favoite places to visit is Nashville because it is a really fun time and I enjoy country music.");
+}
+favoritePlaces()
